252a5c9b8f46ff241ec884288a175fe7bba34b48
Prando <gprando55@gmail.com>
2020-03-24 18:27:47 -0300

    end backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
  
 import OngController from './controllers/OngController';
 import IncidentController from './controllers/IncidentController';
+import ProfileController from './controllers/ProfileController';
+import SessionController from './controllers/SessionController';
 
 const routes = new Router();
 
@@ -12,6 +14,11 @@ routes.post('/ongs', OngController.store);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.store);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.store);
+
 
 export default routes;
 
