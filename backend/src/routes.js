1df12caf8a165821f31abc6dd92fb1c80fec94c9
Prando <gprando55@gmail.com>
2020-03-24 13:25:13 -0300

    config sucrase and eslint

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,13 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hellow Omnistack' }));
+routes.post('/ongs', (req, res) =>{
+  const data = req.body;
 
+  console.log(data);
+  res.json({ message: 'Hellow Omnistack' });
+} 
+)
 
 export default routes;
+
