413d63fe05a419649d4a7e1749939e3a8fde4d88
Gabriel Prando <gprando55@gmail.com>
2020-03-25 17:27:02 -0300

    adicionando frotend

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
 
-import api from '../../services/api';
-import './styles.css';
-import logoImg from '../../assets/logo.svg';
+import "./styles.css";
+
+import api from "../../services/api";
+
+import logoImg from "../../assets/logo.svg";
 
 export default function Register() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [whatsapp, setWhatsapp] = useState('');
-  const [city, setCity] = useState('');
-  const [uf, setUf] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
 
   const history = useHistory();
 
@@ -23,18 +25,19 @@ export default function Register() {
       email,
       whatsapp,
       city,
-      uf,
-    }
+      uf
+    };
+
     try {
+      const response = await api.post("/ongs", data);
 
-      const res = await api.post('ongs', data);
+      alert(`Seu ID de acesso: ${response.data.id}`);
 
-      alert(`Seu ID de acesso é: ${res.data.id}`);
-      history.push('/')
+      history.push("/");
     } catch (err) {
-      alert(`Erro no cadastro`);
+      console.log(err);
+      alert("Erro no cadastro, tente novamente");
     }
-
   }
 
   return (
@@ -43,32 +46,34 @@ export default function Register() {
         <section>
           <img src={logoImg} alt="Be The Hero" />
           <h1>Cadastro</h1>
-          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrar os casos de sua ONG.</p>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrar
+            os casos da sua ONG
+          </p>
 
-          <Link className="back-link" to="/">
-            <FiArrowLeft size={16} color="#E02041" />
-            não tenho cadastro
+          <Link to="/" className="back-link">
+            <FiArrowLeft size={16} color="#e02041" />
+            Não tenho cadastro
           </Link>
         </section>
 
         <form onSubmit={handleRegister}>
           <input
-            placeholder="Nomde da Ong"
+            placeholder="Nome da ONG"
             value={name}
             onChange={e => setName(e.target.value)}
           />
           <input
             type="email"
-            placeholder="e-mail"
+            placeholder="E-mail"
             value={email}
             onChange={e => setEmail(e.target.value)}
           />
           <input
-            placeholder="Whatsapp"
+            placeholder="WhatsApp"
             value={whatsapp}
             onChange={e => setWhatsapp(e.target.value)}
           />
-
           <div className="input-group">
             <input
               placeholder="Cidade"
@@ -83,7 +88,9 @@ export default function Register() {
             />
           </div>
 
-          <button className="button" type="submit">Cadastrar</button>
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
         </form>
       </div>
     </div>
